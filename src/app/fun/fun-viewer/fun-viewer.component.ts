4fc0a8763f88a92260760be721829ee1f584a1aa
Dinu Barbu <dinu@MacBook-Pro.local>
2021-02-26 23:16:41 +0200

    solution complete

--- src/app/fun/fun-viewer/fun-viewer.component.ts
+++ src/app/fun/fun-viewer/fun-viewer.component.ts
@@ -1,18 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChuckNorrisService } from '../chuck-norris.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fun-viewer',
   templateUrl: './fun-viewer.component.html',
   styleUrls: ['./fun-viewer.component.scss'],
 })
-export class FunViewerComponent {
+export class FunViewerComponent implements OnInit {
   @Input() isHorizontal: boolean = false;
 
-  facts$: Observable<string> = this.chuckNorrisSvc.fetchFact();
+  facts$: Observable<string[]> = of([]);
 
   constructor(private chuckNorrisSvc: ChuckNorrisService) {}
+
+  ngOnInit() {
+    this.facts$ = this.chuckNorrisSvc
+      .fetchFact()
+      .pipe(map((fact: string) => new Array(20).fill(fact)));
+  }
 
   toggleLayout() {
     this.isHorizontal = !this.isHorizontal;
