ba5e88942ed89fa8b0125552dfc4839882814378
Dinu Barbu <dinu@MacBook-Pro.local>
2021-02-26 21:36:16 +0200

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FunComponent } from './fun/fun/fun.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FunComponent
   ],
   imports: [
     BrowserModule
