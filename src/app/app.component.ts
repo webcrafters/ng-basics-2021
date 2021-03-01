61c0b419bfcc2781d8638ca0073bda5b9ab7a29c
Diana <devplant@192-168-0-128.rdsnet.ro>
2021-03-01 12:31:09 +0200

    task1 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-basics-2021';
+  // title = 'ng-basics-2021';
+  currentTitle = 'Hello Spring!🌸';
+  allParagraphs = [
+    'I am sitting',
+    'In the morning',
+    'At the diner',
+    'On the corner',
+    'I am waiting',
+    'At the counter',
+    'For the man',
+    'To pour the coffee',
+    'And he fills it',
+    'Only halfway',
+    'And before',
+    'I even argue',
+    'He is looking',
+    'Out the window',
+    'At somebody',
+    'Coming in',
+  ];
 }
