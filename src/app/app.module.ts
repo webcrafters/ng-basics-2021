8a099d9421c9d4d78e5adc22a04353ca4a6a504f
Diana <devplant@192-168-0-128.rdsnet.ro>
2021-03-30 14:44:27 +0300

    angular-material add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DadJokesModule } from './dad-jokes/dad-jokes.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, ChuckNorrisViewerComponent],
+  declarations: [AppComponent, ChuckNorrisViewerComponent, NavbarComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -20,6 +28,13 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     DadJokesModule,
     SharedModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [LoremIpsum],
   bootstrap: [AppComponent],
